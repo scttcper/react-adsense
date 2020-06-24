168ecb717e2796c93c4f56c0d9281b1678b43fec
Scott Cooper <scttcper@gmail.com>
2020-06-23 23:32:24 -0700

    more width

--- demo/App.tsx
+++ demo/App.tsx
@@ -37,7 +37,7 @@ const App = () => {
         </div>
       </div>
       <div className="-mt-16">
-        <div className="max-w-2xl mx-auto px-4 sm:px-6 lg:max-w-3xl lg:px-8">
+        <div className="max-w-2xl mx-auto px-4 sm:px-6 lg:max-w-4xl lg:px-8">
           <div className="bg-white shadow sm:rounded-lg">
             <div className="px-4 py-5 sm:p-6">
               <h3 className="text-lg leading-6 font-medium text-gray-900">Install</h3>
