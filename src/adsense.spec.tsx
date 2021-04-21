78e1b6199fa452a6dfdf39ea45a1561b4f70e999
Scott Cooper <scttcper@gmail.com>
2021-04-20 22:45:21 -0700

    chore: upgrade dependencies (#24)

--- src/adsense.spec.tsx
+++ src/adsense.spec.tsx
@@ -1,17 +1,18 @@
+import '@testing-library/jest-dom/extend-expect';
+
 import React from 'react';
 import { render } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
 
 import { Adsense } from './index';
 
 describe('Adsense', () => {
   it('should be instantiable', () => {
-    const { container } = render(<Adsense client="123" slot="abc" className="ads-class"/>);
+    const { container } = render(<Adsense client="123" slot="abc" className="ads-class" />);
     expect(container.firstChild).toMatchSnapshot();
   });
   it('should call window adsbygoogle', () => {
     (window as any).adsbygoogle = [];
-    render(<Adsense client="123" slot="abc"/>);
+    render(<Adsense client="123" slot="abc" />);
     expect((window as any).adsbygoogle).toHaveLength(1);
     (window as any).adsbygoogle = undefined;
   });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  purge: ['./demo/**/*.{js,jsx,ts,tsx}'],
   theme: {},
   variants: {},
   plugins: [],
