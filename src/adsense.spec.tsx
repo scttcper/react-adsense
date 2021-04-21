9b13266823248f4e2aeb0d49cae66968464e02f0
Scott Cooper <scttcper@gmail.com>
2021-04-20 22:53:15 -0700

    test: add jest globals

--- src/adsense.spec.tsx
+++ src/adsense.spec.tsx
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom/extend-expect';
 
 import React from 'react';
+import { describe, expect, it } from '@jest/globals';
 import { render } from '@testing-library/react';
 
 import { Adsense } from './index';
