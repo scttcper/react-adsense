8930909970adec5a52dafabb7219b95ef41c7839
Scott Cooper <scttcper@gmail.com>
2021-10-05 17:43:00 -0700

    fix: upgrade dep, switch demo to vite (#25)

---
+++ demo/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'media',
+  theme: {},
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  purge: ['./demo/**/*.{js,jsx,ts,tsx}'],
-  theme: {},
-  variants: {},
-  plugins: [],
-};
