ac543e7b0c150fa4d286e3534f624c2bd6fe8d88
Scott Cooper <scttcper@gmail.com>
2022-06-12 19:46:37 -0700

    test: use vitest

--- src/adsense.spec.tsx
+++ src/adsense.spec.tsx
@@ -1,14 +1,20 @@
-import '@testing-library/jest-dom/extend-expect';
-
 import React from 'react';
-import { describe, expect, it } from '@jest/globals';
 import { render } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
 
 import { Adsense } from './index';
 
 describe('Adsense', () => {
   it('should be instantiable', () => {
     const { container } = render(<Adsense client="123" slot="abc" className="ads-class" />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+  it('should allow children', () => {
+    const { container } = render(
+      <Adsense client="123" slot="abc" className="ads-class">
+        <img alt="placeholder" />
+      </Adsense>,
+    );
     expect(container.firstChild).toMatchSnapshot();
   });
   it('should call window adsbygoogle', () => {
